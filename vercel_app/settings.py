15f56ce422ababb23c60b295782aa33292ea0d0e
k <patriotarrow2@gmail.com>
2022-02-20 14:26:13 +0900

    dbchange

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%2!v=#^(#_n4@ihca-rzjl%5@+6ng0nwyl_k+b_v#qp_+f^*iq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'vercel_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
